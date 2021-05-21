37417ca8a0344a8e5bb5a7785fc087a8802f9f6d
velezmidi <velez.midi@gmail.com>
2021-05-21 07:20:54 -0500

    fdfd

---
+++ src/app/mi-componente/mi-componente.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-componente',
+  templateUrl: './mi-componente.component.html',
+  styleUrls: ['./mi-componente.component.scss']
+})
+export class MiComponenteComponent implements OnInit {
+public coso: string;
+  constructor() {
+    this.coso = "panchito";
+   }
+
+  ngOnInit(): void {
+  }
+
+}
